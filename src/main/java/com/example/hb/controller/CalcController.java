a04d4a44cbd447ded17d080dd22b17ae393a5f49
lcy <mysoulli@vip.qq.com>
2018-05-23 14:14:28 +0800

    91秒插入千万级数据

--- src/main/java/com/example/hb/controller/CalcController.java
+++ src/main/java/com/example/hb/controller/CalcController.java
@@ -1,6 +1,7 @@
 package com.example.hb.controller;
 
 import com.example.hb.service.LotteryHistoryService;
+import com.example.hb.service.LotteryStandardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,9 @@ public class CalcController {
     @Autowired
     LotteryHistoryService lotteryHistoryService;
 
+    @Autowired
+    LotteryStandardService lotteryStandardService;
+
     /**
      * 计算所有期望值并保存
      */
@@ -27,6 +31,36 @@ public class CalcController {
             return "计算成功！";
         } catch (Exception e) {
             LOGGER.error("[计算所有期望值]异常：", e);
+            return "系统异常";
+        }
+    }
+
+    /**
+     * 计算历史各项数值并保存
+     */
+    @RequestMapping("/historyData")
+    public String historyData() {
+        try {
+            LOGGER.info("[计算历史各项数据]开始......");
+            lotteryStandardService.calcHistory();
+            return "计算成功！";
+        } catch (Exception e) {
+            LOGGER.error("[计算历史各项数据]异常：", e);
+            return "系统异常";
+        }
+    }
+
+    /**
+     * 计算组合各项数值并保存
+     */
+    @RequestMapping("/combinationData")
+    public String combinationData() {
+        try {
+            LOGGER.info("[计算组合各项数据]开始......");
+            lotteryStandardService.calcCombin();
+            return "计算成功！";
+        } catch (Exception e) {
+            LOGGER.error("[计算组合各项数据]异常：", e);
             return "系统异常";
         }
     }
--- src/main/java/com/example/hb/service/LotteryStandardService.java
+++ src/main/java/com/example/hb/service/LotteryStandardService.java
@@ -1,6 +1,7 @@
 package com.example.hb.service;
 
 import com.example.common.util.CalcUtil;
+import com.example.entity.LotteryCombination;
 import com.example.entity.LotteryHistory;
 import com.example.entity.LotteryStandard;
 import com.example.hb.repository.LotteryCombinationRepository;
@@ -133,13 +134,116 @@ public class LotteryStandardService {
         lotteryStandard.setHistoryRedStandardDiviation(CalcUtil.standardDiviation(rStandardDiviation));
 
         lotteryStandardRepository.save(lotteryStandard);
-
     }
 
     /**
      * 计算所有组合各项数值
      */
     public void calcCombin() {
+        Iterable<LotteryCombination> iterable = lotteryCombinationRepository.findAll();
+        List<LotteryCombination> list = new ArrayList<>();
+        iterable.forEach(single -> {
+            list.add(single);
+        });
 
+        double[] b1Avg = new double[list.size()];
+        double[] b1Variance = new double[list.size()];
+        double[] b1StandardDiviation = new double[list.size()];
+        double[] b1Ccovariance = new double[list.size()];
+
+        double[] b2Avg = new double[list.size()];
+        double[] b2Variance = new double[list.size()];
+        double[] b2StandardDiviation = new double[list.size()];
+        double[] b2Ccovariance = new double[list.size()];
+
+        double[] b3Avg = new double[list.size()];
+        double[] b3Variance = new double[list.size()];
+        double[] b3StandardDiviation = new double[list.size()];
+        double[] b3Ccovariance = new double[list.size()];
+
+        double[] b4Avg = new double[list.size()];
+        double[] b4Variance = new double[list.size()];
+        double[] b4StandardDiviation = new double[list.size()];
+        double[] b4Ccovariance = new double[list.size()];
+
+        double[] b5Avg = new double[list.size()];
+        double[] b5Variance = new double[list.size()];
+        double[] b5StandardDiviation = new double[list.size()];
+        double[] b5Ccovariance = new double[list.size()];
+
+        double[] b6Avg = new double[list.size()];
+        double[] b6Variance = new double[list.size()];
+        double[] b6StandardDiviation = new double[list.size()];
+        double[] b6Ccovariance = new double[list.size()];
+
+        double[] rAvg = new double[list.size()];
+        double[] rVariance = new double[list.size()];
+        double[] rStandardDiviation = new double[list.size()];
+        double[] rCcovariance = new double[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            b1Avg[i] = list.get(i).getBlue1();
+            b1Variance[i] = list.get(i).getBlue1();
+            b1StandardDiviation[i] = list.get(i).getBlue1();
+            b1Ccovariance[i] = list.get(i).getBlue1();
+
+            b2Avg[i] = list.get(i).getBlue2();
+            b2Variance[i] = list.get(i).getBlue2();
+            b2StandardDiviation[i] = list.get(i).getBlue2();
+            b2Ccovariance[i] = list.get(i).getBlue2();
+
+            b3Avg[i] = list.get(i).getBlue3();
+            b3Variance[i] = list.get(i).getBlue3();
+            b3StandardDiviation[i] = list.get(i).getBlue3();
+            b3Ccovariance[i] = list.get(i).getBlue3();
+
+            b4Avg[i] = list.get(i).getBlue4();
+            b4Variance[i] = list.get(i).getBlue4();
+            b4StandardDiviation[i] = list.get(i).getBlue4();
+            b4Ccovariance[i] = list.get(i).getBlue4();
+
+            b5Avg[i] = list.get(i).getBlue5();
+            b5Variance[i] = list.get(i).getBlue5();
+            b5StandardDiviation[i] = list.get(i).getBlue5();
+            b5Ccovariance[i] = list.get(i).getBlue5();
+
+            b6Avg[i] = list.get(i).getBlue6();
+            b6Variance[i] = list.get(i).getBlue6();
+            b6StandardDiviation[i] = list.get(i).getBlue6();
+            b6Ccovariance[i] = list.get(i).getBlue6();
+
+            rAvg[i] = list.get(i).getRed();
+            rVariance[i] = list.get(i).getRed();
+            rStandardDiviation[i] = list.get(i).getRed();
+            rCcovariance[i] = list.get(i).getRed();
+        }
+
+        LotteryStandard lotteryStandard = new LotteryStandard();
+        //计算均值
+        lotteryStandard.setCombinBlue1Avg(CalcUtil.avg(b1Avg));
+        lotteryStandard.setCombinBlue2Avg(CalcUtil.avg(b2Avg));
+        lotteryStandard.setCombinBlue3Avg(CalcUtil.avg(b3Avg));
+        lotteryStandard.setCombinBlue4Avg(CalcUtil.avg(b4Avg));
+        lotteryStandard.setCombinBlue5Avg(CalcUtil.avg(b5Avg));
+        lotteryStandard.setCombinBlue6Avg(CalcUtil.avg(b6Avg));
+        lotteryStandard.setCombinRedAvg(CalcUtil.avg(rAvg));
+        //计算方差
+        lotteryStandard.setCombinBlue1Variance(CalcUtil.variance(b1Variance));
+        lotteryStandard.setCombinBlue2Variance(CalcUtil.variance(b2Variance));
+        lotteryStandard.setCombinBlue3Variance(CalcUtil.variance(b3Variance));
+        lotteryStandard.setCombinBlue4Variance(CalcUtil.variance(b4Variance));
+        lotteryStandard.setCombinBlue5Variance(CalcUtil.variance(b5Variance));
+        lotteryStandard.setCombinBlue6Variance(CalcUtil.variance(b6Variance));
+        lotteryStandard.setCombinRedVariance(CalcUtil.variance(rVariance));
+        //计算标准差
+        lotteryStandard.setCombinBlue1StandardDiviation(CalcUtil.standardDiviation(b1StandardDiviation));
+        lotteryStandard.setCombinBlue2StandardDiviation(CalcUtil.standardDiviation(b2StandardDiviation));
+        lotteryStandard.setCombinBlue3StandardDiviation(CalcUtil.standardDiviation(b3StandardDiviation));
+        lotteryStandard.setCombinBlue4StandardDiviation(CalcUtil.standardDiviation(b4StandardDiviation));
+        lotteryStandard.setCombinBlue5StandardDiviation(CalcUtil.standardDiviation(b5StandardDiviation));
+        lotteryStandard.setCombinBlue6StandardDiviation(CalcUtil.standardDiviation(b6StandardDiviation));
+        lotteryStandard.setCombinRedStandardDiviation(CalcUtil.standardDiviation(rStandardDiviation));
+
+        lotteryStandardRepository.save(lotteryStandard);
     }
 }
